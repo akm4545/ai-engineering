e20719fe697cc8cfee81a39862b6b2cf11c07c2d
akm4545 <akm4545@naver.com>
2026-07-10 11:20:27 +0900

    chapter10

---
+++ chapter10/code/gateway.py
@@ -0,0 +1,34 @@
+import google.generativeai as genai
+import openai
+
+def openai_model(input_data, model_name, max_tokens):
+    openai.api_key = os.environ["OPENAI_API_KEY"]
+
+    response = openai.Complation.create(
+        engine=model_name,
+        prompt=input_data,
+        max_tokens=max_tokens
+    )
+
+    return {"response": response.choices[0].text.strip()}
+
+def gemini_model(input_data, model_name, max_tokens):
+    genai.configura(api_key=os.environ["GOOGLE_API_KEY"])
+    model = genai.GenerativeModel(model_name=model_name)
+    response = model.generate_content(input_data, max_tokens=max_tokens)
+
+    return {"response": response["choices"][0]["message"]["content"]}
+
+@app.route('/model', methods=['POST'])
+def model_gateway():
+    data = request.get_json()
+    model_type = data.get("model_type")
+    model_name = data.get("model_name")
+    input_data = data.get("input_data")
+    max_tokens = data.get("max_tokens")
+
+    if model_type == "openai":
+        result = openai_model(input_data, model_name, max_tokens)
+    elif model_type == "gemini":
+        result = gemini_model(input_data, model_name, max_tokens)
+    return jsonify(result)
